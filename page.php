5c3be841ba8d3bb98dda9158867ea6cdb667a90f
Christopher Finke <chris@efinke.com>
2014-01-15 18:58:57 +0000

    Basic page styling.

--- page.php
+++ page.php
@@ -11,25 +11,18 @@
  */
 
 get_header(); ?>
+<li class="header"><?php the_title(); ?></li>
+<?php while ( have_posts() ) : the_post(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<?php get_template_part( 'content' ); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<?php
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
+	?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+<?php endwhile; // end of the loop. ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

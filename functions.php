71eb56c13c4c83dc6ea31c873b8368c13efec44c
Christopher Finke <chris@efinke.com>
2014-01-15 18:59:36 +0000

    No menus.

--- functions.php
+++ functions.php
@@ -40,11 +40,6 @@ function keyring_river_setup() {
 	 */
 	//add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'keyring-river' ),
-	) );
-
 	// Enable support for Post Formats.
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
 

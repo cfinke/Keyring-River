585c17a0eff9db102e3e5c2740ec10bb3f39e358
Christopher Finke <chris@efinke.com>
2014-01-15 18:54:20 +0000

    Archive page stylinh.

--- archive.php
+++ archive.php
@@ -8,98 +8,88 @@
  */
 
 get_header(); ?>
+<?php if ( have_posts() ) : ?>
+	<li class="header">
+		<?php
+			if ( is_category() ) :
+				single_cat_title();
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+			elseif ( is_tag() ) :
+				single_tag_title();
 
-		<?php if ( have_posts() ) : ?>
+			elseif ( is_author() ) :
+				printf( __( 'Author: %s', 'keyring-river' ), '<span class="vcard">' . get_the_author() . '</span>' );
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
+			elseif ( is_day() ) :
+				printf( __( 'Day: %s', 'keyring-river' ), '<span>' . get_the_date() . '</span>' );
 
-						elseif ( is_tag() ) :
-							single_tag_title();
+			elseif ( is_month() ) :
+				printf( __( 'Month: %s', 'keyring-river' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'keyring-river' ) ) . '</span>' );
 
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'keyring-river' ), '<span class="vcard">' . get_the_author() . '</span>' );
+			elseif ( is_year() ) :
+				printf( __( 'Year: %s', 'keyring-river' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'keyring-river' ) ) . '</span>' );
 
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'keyring-river' ), '<span>' . get_the_date() . '</span>' );
+			elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+				_e( 'Asides', 'keyring-river' );
 
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'keyring-river' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'keyring-river' ) ) . '</span>' );
+			elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+				_e( 'Galleries', 'keyring-river');
 
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'keyring-river' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'keyring-river' ) ) . '</span>' );
+			elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+				_e( 'Images', 'keyring-river');
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'keyring-river' );
+			elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+				_e( 'Videos', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'keyring-river');
+			elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+				_e( 'Quotes', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'keyring-river');
+			elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+				_e( 'Links', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'keyring-river' );
+			elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+				_e( 'Statuses', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'keyring-river' );
+			elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+				_e( 'Audios', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'keyring-river' );
+			elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+				_e( 'Chats', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'keyring-river' );
+			else :
+				_e( 'Archives', 'keyring-river' );
 
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'keyring-river' );
+			endif;
+		?>
+	</li>
+		<?php
+			// Show an optional term description.
+			$term_description = term_description();
+			if ( ! empty( $term_description ) ) :
+				printf( '<li class="header">%s</li>', $term_description );
+			endif;
+		?>
+	</header><!-- .page-header -->
 
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'keyring-river' );
+	<?php /* Start the Loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-						else :
-							_e( 'Archives', 'keyring-river' );
+		<?php
+			/* Include the Post-Format-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
+			get_template_part( 'content', get_post_format() );
+		?>
 
-						endif;
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
+	<?php endwhile; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+	<?php keyring_river_paging_nav(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+<?php else : ?>
 
-			<?php endwhile; ?>
+	<?php get_template_part( 'content', 'none' ); ?>
 
-			<?php keyring_river_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
+<?php endif; ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
